527640ee6bfa32873b8e2fe907b349a27f178a27
baaami <lgh121546@naver.com>
2024-06-04 08:23:16 +0900

    개방 폐쇄 원칙 예시 직접 구현
    - **설명:** 소프트웨어 요소는 확장에는 열려 있어야 하고, 변경에는 닫혀 있어야 해요.
    - **이유:** 새로운 기능을 추가할 때 기존 코드를 수정하지 않으면 오류 발생 가능성이 줄어들어요.
    - **예시:** 기존 클래스의 기능을 변경하지 않고 상속이나 인터페이스 구현을 통해 새로운 기능을 추가할 수 있어야 해요.
    
    [의견]
    
    - 아래 코드 예시의 경우 개방-폐쇄 원칙에 위반하였을 경우 새로운 항목 추가 시 CalculateTotalArea 함수를 수정해야한다.
    - 플러그인 아키텍처가 개방-폐쇄 원칙의 좋은 예시로 볼 수 있음 기존 시스템 수정 없이 새로운 도구들을 확장해나갈 수 있음
    - 큰 범위의 interface 가 존재할  때 여집합이 존재하는 interface들을 만들어 주어야 코드가 유연해지고 재사용성이 높아질 수 있다.

---
+++ design/oop/Open_Closed_Principle/unuse/main.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width, Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return 3.14 * c.Radius * c.Radius
+}
+
+// 삼각형 추가 시 다음과 같이 추가 가능
+type Triangle struct {
+	Base, Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
+
+type AreaCalculator struct{}
+
+func (ac AreaCalculator) CalculateTotalArea(shapes []Shape) float64 {
+	var totalArea float64
+	for _, shape := range shapes {
+		switch s := shape.(type) {
+		case Rectangle:
+			totalArea += s.Area()
+		case Circle:
+			totalArea += s.Area()
+		case Triangle:
+			totalArea += s.Area()
+		}
+	}
+	return totalArea
+}
+
+func main() {
+	shapes := []Shape{
+		Rectangle{Width: 3, Height: 4},
+		Circle{Radius: 5},
+		Triangle{Base: 6, Height: 7},
+	}
+	calculator := AreaCalculator{}
+	fmt.Println("Total Area:", calculator.CalculateTotalArea(shapes))
+}
